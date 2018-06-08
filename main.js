da36f2533de3105bd1d79a398040b31c6b735acc
Tasha <tamitchell14@gmail.com>
2018-06-08 07:12:07 -0400

    added lost changes

---
+++ main.js
@@ -0,0 +1,26 @@
+var cards = ['queen', 'queen', 'king', 'king'];
+    var cardsInPlay = [];
+    var cardOne;
+    cardOne = cards[0];
+    cardsInPlay.push(cardOne);
+    
+    var cardTwo;
+    cardTwo = cards[2];
+    cardsInPlay.push(cardTwo);
+
+    console.log("User flipped " + cardOne);
+    console.log("User flipped " + cardTwo);
+
+ if (cardsInPlay.length === 2) {
+   if (cardsInPlay[0] === cardsInPlay[2]) {
+     alert("You found a match!");
+   }
+   else {
+     alert("Sorry, try again!");
+   }
+ }
+
+
+
+
+
